d5ac661c6733bf46043915ce3196186142efc350
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-27 02:33:12 -0300

    Fixes label warnings, watch not working

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -1,49 +1,60 @@
 <template>
-	<div>
-		<form v-on:submit.prevent="onFormSubmit">
-			<div>
-				<p>
-					<label for="productName">Nome: </label>
-					<input type="text" v-model="productName" />
-				</p>
-			</div>
-			<p>
-				<label for="productPrice">Preço: </label>
-				<input type="text" v-model="productPrice" />
-			</p>
-			<p>
-				<label for="productCategory">Categoria: </label>
-				<input type="text" v-model="productCategory" />
-			</p>
-			<button type="submit">Enviar</button>
-		</form>
-		<p>{{ productName }} {{ productPrice }} {{ productCategory }}</p>
-	</div>
+  <div>
+    <form v-on:submit.prevent="onFormSubmit">
+      <div>
+        <p>
+          <label for="productName">Nome: </label>
+          <input id="productName" v-model="productName" type="text"/>
+        </p>
+      </div>
+      <p>
+        <label for="productPrice">Preço: </label>
+        <input id="productPrice" v-model="productPrice" type="text"/>
+      </p>
+      <p>
+        <label for="productCategory">Categoria: </label>
+        <input id="productCategory" v-model="productCategory" type="text"/>
+      </p>
+      <button type="submit">Enviar</button>
+    </form>
+    <p>{{ productName }} {{ productPrice }} {{ productCategory }}</p>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'ProductForm',
-		data() {
-			return {
-				productName: '',
-				productPrice: '',
-				productCategory: '',
-			};
-		},
-		props: {
-			onAddProduct: Function,
-		},
-		methods: {
-			onFormSubmit() {
-				const product = {
-					productName: this.productName,
-					productPrice: this.productPrice,
-					productCategory: this.productCategory,
-				};
+export default {
+  name: 'ProductForm',
+  data() {
+    return {
+      productName: '',
+      productPrice: '',
+      productCategory: '',
+      isNameLimitExceeded: '',
+    };
+  },
+  props: {
+    onAddProduct: Function,
+  },
+  methods: {
+    onFormSubmit() {
+      const product = {
+        productName: this.productName,
+        productPrice: this.productPrice,
+        productCategory: this.productCategory,
+      };
 
-				this.onAddProduct(product);
-			},
-		},
-	};
+      this.onAddProduct(product);
+    },
+  },
+  watch: {
+    productName() {
+      if (this.productName.length > 15) {
+        this.isNameLimitExceeded = 'O nome\n' +
+            'do produto não pode ultrapassar os 15 caracteres';
+      } else {
+        this.isNameLimitExceeded = '';
+      }
+    },
+  },
+};
 </script>
