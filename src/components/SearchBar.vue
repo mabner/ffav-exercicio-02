b46360be472a5e43133dea4f70350e4470827714
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-27 00:54:58 -0300

    SearchBar file

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<form v-on:submit.prevent="handleSearch">
+		<input type="text" v-on:keyup="handleSearch" v-model="search" />
+		<button type="submit">Search</button>
+	</form>
+</template>
+
+<script>
+	export default {
+		name: 'SearchBar',
+		props: {
+            onSearch: Function,
+        },
+		data() {
+            return {
+                search: "",
+            };
+        },
+		methods: {
+            handleSearch() {
+                this.onSearch(this.search);
+            }
+        },
+	};
+</script>
